6de141d949b10725cc1531a1e333e94f89b1d7fd
Davi Bauer <davibauer@gmail.com>
2022-09-11 22:11:21 -0300

    Added new stake contract

--- toyo-mumbai/src/mapping.ts
+++ toyo-mumbai/src/mapping.ts
@@ -1,19 +1,24 @@
-import { BigInt, json, JSONValueKind, Address } from "@graphprotocol/graph-ts"
+import {
+  BigInt,
+  json,
+  JSONValueKind,
+  Address,
+  Bytes,
+} from "@graphprotocol/graph-ts";
 
 import {
   TokenPaused,
   TokenPurchased,
   TokenTypeAdded,
-} from "../generated/NftTokenCrowdsale-v1/NftTokenCrowdsale"
+} from "../generated/NftTokenCrowdsale-v1/NftTokenCrowdsale";
 
-import {
-  TokenSwapped
-} from "../generated/NftTokenSwap-v1/NftTokenSwap"
+import { TokenSwapped } from "../generated/NftTokenSwap-v1/NftTokenSwap";
 
-import {
-  NftToken,
-  Transfer
-} from "../generated/NftToken-v1/NftToken"
+import { NftToken, Transfer } from "../generated/NftToken-v1/NftToken";
+
+import { TokenStaked } from "../generated/NftTokenStake-v1/NftTokenStake";
+
+import { TokenClaimed } from "../generated/NftTokenStake-v2/NftTokenStake";
 
 import {
   TokenPausedEntity,
@@ -22,16 +27,18 @@ import {
   TokenOwnerEntity,
   TokenSwappedEntity,
   TokenTransferEntity,
-} from "../generated/schema"
+  TokenStakedEntity,
+  TokenClaimedEntity,
+} from "../generated/schema";
 
 export function handleTokenPaused(event: TokenPaused): void {
-  let entity = TokenPausedEntity.load(event.transaction.hash.toHex())
+  let entity = TokenPausedEntity.load(event.transaction.hash.toHex());
   if (entity == null) {
-    entity = new TokenPausedEntity(event.transaction.hash.toHex())
+    entity = new TokenPausedEntity(event.transaction.hash.toHex());
   }
-  entity.changer = event.params.changer
-  entity.typeId = event.params.typeId
-  entity.save()
+  entity.changer = event.params.changer;
+  entity.typeId = event.params.typeId;
+  entity.save();
 }
 
 export function handleTokenPurchased(event: TokenPurchased): void {
@@ -39,97 +46,149 @@ export function handleTokenPurchased(event: TokenPurchased): void {
   let entityId = event.transaction.hash.toHex() + tokenId.toString();
 
   /* Purchase */
-  let purchased = TokenPurchasedEntity.load(entityId)
+  let purchased = TokenPurchasedEntity.load(entityId);
   if (purchased == null) {
-    purchased = new TokenPurchasedEntity(entityId)
+    purchased = new TokenPurchasedEntity(entityId);
   }
-  purchased.beneficiary = event.params.beneficiary
-  purchased.spender = event.params.spender
-  purchased.tokenId = tokenId
-  purchased.totalSupply = event.params.totalSupply
-  purchased.typeId = event.params.typeId
-  purchased.value = event.params.value
-  purchased.blockNumber = event.block.number
-  purchased.blockTimestamp = event.block.timestamp
-  purchased.transactionHash = event.transaction.hash
-  
-  purchased.save()
+  purchased.beneficiary = event.params.beneficiary;
+  purchased.spender = event.params.spender;
+  purchased.tokenId = tokenId;
+  purchased.totalSupply = event.params.totalSupply;
+  purchased.typeId = event.params.typeId;
+  purchased.value = event.params.value;
+  purchased.blockNumber = event.block.number;
+  purchased.blockTimestamp = event.block.timestamp;
+  purchased.transactionHash = event.transaction.hash;
 
-  let type = TokenTypeEntity.load(event.params.typeId.toString())
+  purchased.save();
+
+  let type = TokenTypeEntity.load(event.params.typeId.toString());
   if (type != null) {
-    type.count = type.count + BigInt.fromI32(1)
-    type.save()
+    type.count = type.count.plus(BigInt.fromI32(1));
+    type.save();
   }
 
-  let owner = TokenOwnerEntity.load(tokenId.toString())
+  let owner = TokenOwnerEntity.load(tokenId.toString());
   if (owner == null) {
-    owner = new TokenOwnerEntity(tokenId.toString())
+    owner = new TokenOwnerEntity(tokenId.toString());
   }
 
   /* Owner */
-  owner.currentOwner = event.params.beneficiary
-  owner.tokenId = tokenId
-  owner.typeId = event.params.typeId
-  owner.blockNumber = event.block.number
-  owner.blockTimestamp = event.block.timestamp
-  owner.transactionHash = event.transaction.hash
-  owner.save()
+  owner.currentOwner = event.params.beneficiary;
+  owner.currentOwnerOrStaker = event.params.beneficiary;
+  owner.isStaked = false;
+  owner.tokenId = tokenId;
+  owner.typeId = event.params.typeId;
+  owner.blockNumber = event.block.number;
+  owner.blockTimestamp = event.block.timestamp;
+  owner.transactionHash = event.transaction.hash;
+  owner.save();
 }
 
 export function handleTokenTypeAdded(event: TokenTypeAdded): void {
-  let entity = TokenTypeEntity.load(event.params.newValue.toString())
-  if (entity == null) {
-    entity = new TokenTypeEntity(event.params.newValue.toString())
+  let type = TokenTypeEntity.load(event.params.newValue.toString());
+  if (type == null) {
+    type = new TokenTypeEntity(event.params.newValue.toString());
   }
-  entity.changer = event.params.changer
-  entity.newValue = event.params.newValue
-  entity.count = BigInt.fromI32(0)
-  entity.save()
+  type.changer = event.params.changer;
+  type.newValue = event.params.newValue;
+  type.count = BigInt.fromI32(0);
+  type.save();
 }
 
 export function handleTokenSwapped(event: TokenSwapped): void {
   let tokenId = event.params.toTokenId;
   let entityId = event.transaction.hash.toHex() + tokenId.toString();
 
-  let entity = TokenSwappedEntity.load(entityId)
-  if (entity == null) {
-    entity = new TokenSwappedEntity(entityId)
+  let swapped = TokenSwappedEntity.load(entityId);
+  if (swapped == null) {
+    swapped = new TokenSwappedEntity(entityId);
   }
-  entity.sender = event.params.sender
-  entity.fromTypeId = event.params.fromTypeId
-  entity.toTypeId = event.params.toTypeId
-  entity.fromTokenId = event.params.fromTokenId
-  entity.toTokenId = event.params.toTokenId
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-  entity.save()
+  swapped.sender = event.params.sender;
+  swapped.fromTypeId = event.params.fromTypeId;
+  swapped.toTypeId = event.params.toTypeId;
+  swapped.fromTokenId = event.params.fromTokenId;
+  swapped.toTokenId = event.params.toTokenId;
+  swapped.blockNumber = event.block.number;
+  swapped.blockTimestamp = event.block.timestamp;
+  swapped.transactionHash = event.transaction.hash;
+  swapped.save();
 }
 
 export function handleTransfer(event: Transfer): void {
   /* Transfer */
-  let transfer = TokenTransferEntity.load(event.transaction.hash.toHex())
+  let transfer = TokenTransferEntity.load(event.transaction.hash.toHex());
   if (transfer == null) {
-    transfer = new TokenTransferEntity(event.transaction.hash.toHex())
+    transfer = new TokenTransferEntity(event.transaction.hash.toHex());
   }
-  transfer.from = event.params.from
-  transfer.to = event.params.to
-  transfer.tokenId = event.params.tokenId
-  transfer.blockNumber = event.block.number
-  transfer.blockTimestamp = event.block.timestamp
-  transfer.transactionHash = event.transaction.hash
-  transfer.save()
+  transfer.from = event.params.from;
+  transfer.to = event.params.to;
+  transfer.tokenId = event.params.tokenId;
+  transfer.blockNumber = event.block.number;
+  transfer.blockTimestamp = event.block.timestamp;
+  transfer.transactionHash = event.transaction.hash;
+  transfer.save();
 
   /* Owner */
-  let owner = TokenOwnerEntity.load(event.params.tokenId.toString())
+  let owner = TokenOwnerEntity.load(event.params.tokenId.toString());
   if (owner == null) {
-    owner = new TokenOwnerEntity(event.params.tokenId.toString())
+    owner = new TokenOwnerEntity(event.params.tokenId.toString());
+    owner.isStaked = false;
   }
 
-  owner.currentOwner = event.params.to
-  owner.tokenId = event.params.tokenId
-  owner.blockNumber = event.block.number
-  owner.blockTimestamp = event.block.timestamp
-  owner.transactionHash = event.transaction.hash
-  owner.save()
+  owner.currentOwner = event.params.to;
+  if (!owner.isStaked) {
+    owner.currentOwnerOrStaker = event.params.to;
+  }
+  owner.tokenId = event.params.tokenId;
+  owner.blockNumber = event.block.number;
+  owner.blockTimestamp = event.block.timestamp;
+  owner.transactionHash = event.transaction.hash;
+  owner.save();
+}
+
+export function handleTokenStaked(event: TokenStaked): void {
+  let stake = TokenStakedEntity.load(event.transaction.hash.toHex());
+  if (stake == null) {
+    stake = new TokenStakedEntity(event.transaction.hash.toHex());
+  }
+  stake.currentOwner = event.params.owner;
+  stake.tokenId = event.params.tokenId;
+  stake.save();
+
+  /* Owner */
+  let owner = TokenOwnerEntity.load(event.params.tokenId.toString());
+  if (owner == null) {
+    owner = new TokenOwnerEntity(event.params.tokenId.toString());
+  }
+
+  owner.currentStaker = event.params.owner;
+  owner.currentOwnerOrStaker = event.params.owner;
+  owner.isStaked = true;
+  owner.save();
+}
+
+export function handleTokenClaimed(event: TokenClaimed): void {
+  let claim = TokenClaimedEntity.load(event.transaction.hash.toHex());
+  if (claim == null) {
+    claim = new TokenClaimedEntity(event.transaction.hash.toHex());
+  }
+
+  claim.claimId = event.params.claimId.toString();
+  claim.owner = event.params.owner;
+  claim.tokenId = event.params.tokenId;
+  claim.bondAmount = event.params.bondAmount;
+  claim.cardCode = event.params.cardCode;
+  claim.save();
+
+  /* Owner */
+  let owner = TokenOwnerEntity.load(event.params.tokenId.toString());
+  if (owner == null) {
+    owner = new TokenOwnerEntity(event.params.tokenId.toString());
+  }
+
+  owner.currentStaker = null;
+  owner.currentOwnerOrStaker = event.params.owner;
+  owner.isStaked = false;
+  owner.save();
 }
